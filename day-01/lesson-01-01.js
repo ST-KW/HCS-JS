167784d1145dd7058cd4d5777473ca4565ce72ad
ST-KW <stephan@kambor-wiesenberg.de>
2019-09-01 18:55:21 +0200

    [TA-453] console.log

---
+++ day-01/lesson-01-01.js
@@ -0,0 +1,12 @@
+// Console output
+
+console.log("Hello World!");
+consle.log("Hello World!");
+Console.log("Hello World!");
+console.log(Hello World!);
+console.log('Hello World!'); // both works ' or " but don't mix works
+console.log("Hello World!") // ; is optional
+
+console.log("Hello World!");
+console.log("How are you?");
+console.log("Fine. Thanks.");
